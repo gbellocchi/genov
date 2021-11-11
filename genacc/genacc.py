29a423daa56a6fc8a61bbdf6345ef5757bce4d4c
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2021-11-11 12:00:19 +0100

    little update on generation

--- genacc/genacc.py
+++ genacc/genacc.py
@@ -6,6 +6,11 @@
 from generator import generator
 from emitter import emitter
 
+'''
+    Import templates
+    =========
+'''
+
 # HW packages
 from templates.hw.hwpe_wrapper.hwpe_wrapper import hwpe_wrapper
 from templates.hw.overlay.overlay import overlay
@@ -17,6 +22,19 @@ from templates.integr_support.integr_support import integr_support
 # SW packages
 from templates.sw.hwpe_ov_tb.hwpe_ov_tb import hwpe_ov_tb as hwpe_ov_tb_sw
 from templates.sw.hwpe_standalone_tb.hwpe_standalone_tb import hwpe_standalone_tb as hwpe_standalone_tb_sw
+
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
+
+'''
+    Components generator
+    ====================
+'''
+
+def gencomps(temp_obj, descr, out_dir):
+    template = temp_obj
+    out_target = generator.render(template)
+    filename = emitter.get_file_name(descr)
+    emitter.out_gen(out_target, filename, out_dir)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -58,8 +76,8 @@ emitter.out_sw_static()
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
 '''
-    Hardware wrapper
-    ================
+    Hardware wrapper (HWPE-based)
+    =============================
 
     Instantiate HWPE wrapper item
 '''
@@ -68,66 +86,75 @@ hwpe_wrapper = hwpe_wrapper()
 '''
     Generate design components ~ Top wrapper
 '''
-template = hwpe_wrapper.top_wrapper()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'top_wrapper', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_wrap)
+gencomps(
+    hwpe_wrapper.top_wrapper(),
+    ['hwpe', 'top_wrapper', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_wrap
+)
+
 
 '''
     Generate design components ~ Top
 '''
-template = hwpe_wrapper.top()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'top', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.top(),
+    ['hwpe', 'top', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ Engine
 '''
-template = hwpe_wrapper.engine()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'engine', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.engine(),
+    ['hwpe', 'engine', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ Kernel adapter
 '''
-template = hwpe_wrapper.kernel_adapter()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'kernel_adapter', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.kernel_adapter(),
+    ['hwpe', 'kernel_adapter', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ Streamer
 '''
-template = hwpe_wrapper.streamer()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'streamer', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.streamer(),
+    ['hwpe', 'streamer', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ Controller
 '''
-template = hwpe_wrapper.ctrl()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'ctrl', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.ctrl(),
+    ['hwpe', 'ctrl', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ FSM
 '''
-template = hwpe_wrapper.fsm()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'fsm', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.fsm(),
+    ['hwpe', 'fsm', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 '''
     Generate design components ~ Package
 '''
-template = hwpe_wrapper.package()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['hwpe', 'package', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_hwpe_rtl)
+gencomps(
+    hwpe_wrapper.package(),
+    ['hwpe', 'package', ['hw', 'rtl']],
+    emitter.out_hw_hwpe_rtl
+)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -142,18 +169,20 @@ overlay = overlay()
 '''
     Generate design components ~ Overlay accelerator package
 ''' 
-template = overlay.acc_pkg()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['ov', 'acc_pkg', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_ov)
+gencomps(
+    overlay.acc_pkg(),
+    ['ov', 'acc_pkg', ['hw', 'rtl']],
+    emitter.out_hw_ov
+)
 
 '''
     Generate design components ~ Overlay accelerator interface
 ''' 
-template = overlay.acc_intf()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['ov', 'acc_intf', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_ov)
+gencomps(
+    overlay.acc_intf(),
+    ['ov', 'acc_intf', ['hw', 'rtl']],
+    emitter.out_hw_ov
+)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -172,10 +201,11 @@ hwpe_standalone_tb_hw = hwpe_standalone_tb_hw()
     dummy memories to implement instruction, stack and data
     memories.
 ''' 
-template = hwpe_standalone_tb_hw.tb_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['tb', 'tb_hwpe', ['hw', 'rtl']])
-emitter.out_gen(out_target, filename, emitter.out_hw_standalone_tb)
+gencomps(
+    hwpe_standalone_tb_hw.tb_hwpe(),
+    ['ov', 'tb_hwpe', ['hw', 'rtl']],
+    emitter.out_hw_standalone_tb
+)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -190,18 +220,20 @@ integr_support = integr_support()
 '''
     Generate design components ~ Bender
 ''' 
-template = integr_support.bender()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['integr_support', 'Bender', ['integr_support', 'bender']])
-emitter.out_gen(out_target, filename, emitter.out_ov_integr)
+gencomps(
+    integr_support.bender(),
+    ['integr_support', 'Bender', ['integr_support', 'bender']],
+    emitter.out_ov_integr
+)
 
 '''
     Generate design components ~ QuestaSim waves
 ''' 
-template = integr_support.vsim_wave()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['integr_support', 'pulp_tb', ['integr_support', 'vsim_wave']])
-emitter.out_gen(out_target, filename, emitter.out_ov_integr)
+gencomps(
+    integr_support.vsim_wave(),
+    ['integr_support', 'pulp_tb', ['integr_support', 'vsim_wave']],
+    emitter.out_ov_integr
+)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -217,10 +249,11 @@ hwpe_ov_tb_sw = hwpe_ov_tb_sw()
     Generate design components ~ archi
     Retrieve memory-mapped hardware accelerator registers.
 ''' 
-template = hwpe_ov_tb_sw.archi_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'archi_hwpe', ['sw', 'archi']])
-emitter.out_gen(out_target, filename, emitter.out_sw_ov_hwpe_lib)
+gencomps(
+    hwpe_ov_tb_sw.archi_hwpe(),
+    ['sw', 'archi_hwpe', ['sw', 'archi']],
+    emitter.out_sw_ov_hwpe_lib
+)
 
 '''
     Generate design components ~ hardware abstraction layer (HAL)
@@ -228,10 +261,11 @@ emitter.out_gen(out_target, filename, emitter.out_sw_ov_hwpe_lib)
     to create an interaction between the RISC-V processor and the 
     hardware accelerator.
 ''' 
-template = hwpe_ov_tb_sw.hal_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'hal_hwpe', ['sw', 'hal']])
-emitter.out_gen(out_target, filename, emitter.out_sw_ov_hwpe_lib)
+gencomps(
+    hwpe_ov_tb_sw.hal_hwpe(),
+    ['sw', 'hal_hwpe', ['sw', 'hal']],
+    emitter.out_sw_ov_hwpe_lib
+)
 
 '''
     Generate design components ~ software testbench 
@@ -240,10 +274,11 @@ emitter.out_gen(out_target, filename, emitter.out_sw_ov_hwpe_lib)
     in the overlay system. This tb can be used as a starting point for 
     additional platform testing.
 ''' 
-template = hwpe_ov_tb_sw.tb_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'tb_hwpe', ['sw', 'tb']])
-emitter.out_gen(out_target, filename, emitter.out_sw_ov)
+gencomps(
+    hwpe_ov_tb_sw.tb_hwpe(),
+    ['sw', 'tb_hwpe', ['sw', 'tb']],
+    emitter.out_sw_ov
+)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
 
@@ -259,10 +294,11 @@ hwpe_standalone_tb_sw = hwpe_standalone_tb_sw()
     Generate design components ~ archi
     Retrieve memory-mapped hardware accelerator registers.
 ''' 
-template = hwpe_standalone_tb_sw.archi_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'archi_hwpe', ['sw', 'archi']])
-emitter.out_gen(out_target, filename, emitter.out_sw_standalone_hwpe_lib)
+gencomps(
+    hwpe_standalone_tb_sw.archi_hwpe(),
+    ['sw', 'archi_hwpe', ['sw', 'archi']],
+    emitter.out_sw_standalone_hwpe_lib
+)
 
 '''
     Generate design components ~ hardware abstraction layer (HAL)
@@ -270,10 +306,11 @@ emitter.out_gen(out_target, filename, emitter.out_sw_standalone_hwpe_lib)
     to create an interaction between the RISC-V processor and the 
     hardware accelerator.
 ''' 
-template = hwpe_standalone_tb_sw.hal_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'hal_hwpe', ['sw', 'hal']])
-emitter.out_gen(out_target, filename, emitter.out_sw_standalone_hwpe_lib)
+gencomps(
+    hwpe_standalone_tb_sw.hal_hwpe(),
+    ['sw', 'hal_hwpe', ['sw', 'hal']],
+    emitter.out_sw_standalone_hwpe_lib
+)
 
 '''
     Generate design components ~ software testbench 
@@ -283,7 +320,8 @@ emitter.out_gen(out_target, filename, emitter.out_sw_standalone_hwpe_lib)
     functionality of the generated wrapper before to integrate it at 
     system-level.
 ''' 
-template = hwpe_standalone_tb_sw.tb_hwpe()
-out_target = generator.render(template)
-filename = emitter.get_file_name(['sw', 'tb_hwpe', ['sw', 'tb']])
-emitter.out_gen(out_target, filename, emitter.out_sw_standalone)
+gencomps(
+    hwpe_standalone_tb_sw.tb_hwpe(),
+    ['sw', 'tb_hwpe', ['sw', 'tb']],
+    emitter.out_sw_standalone
+)
