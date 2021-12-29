7880b8c72057d452549fcbd9334db130e531e133
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2021-12-29 17:45:31 +0100

    removed generic ov_acc_intf and substituted with shared_lic_acc_region (then also other two variants will come after this). the generation has been tested with three accelerators. documentation has been added and template APIs have been standardized (see Note notes).

---
+++ genov/templates/ov_templ/hw/overlay/shared_lic_acc_region/top/shared_lic_acc_region.py
@@ -0,0 +1,25 @@
+'''
+ =====================================================================
+ Project:       Shared LIC accelerator region
+ Title:         shared_lic_acc_region.py
+ Description:   The accelerator region is connected to the overlay 
+                infrastructure by means of a shared logarithmic 
+                interconnect. This module also instantiates the 
+                accelerator interfaces to the accelerator wrappers.
+
+ Date:          29.12.2021
+ ===================================================================== */
+
+ Copyright (C) 2021 University of Modena and Reggio Emilia.
+
+ Author: Gianluca Bellocchi, University of Modena and Reggio Emilia.
+
+'''
+
+#!/usr/bin/env python3
+
+from collector import collector
+
+class shared_lic_acc_region(collector):
+    def top(self):
+        return self.get_template()
