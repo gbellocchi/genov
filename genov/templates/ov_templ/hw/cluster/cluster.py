887e532a0a1f0d0579d559d8fa17ac90e9acbc28
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-01-13 23:29:27 +0100

    added new templates for cluster.

--- genov/templates/ov_templ/hw/cluster/cluster.py
+++ genov/templates/ov_templ/hw/cluster/cluster.py
@@ -3,7 +3,7 @@
  Project:       Cluster class
  Title:         cluster.py
  Description:   The Cluster class recalls all the the templates 
-                compiant with the generation of an overlay cluster tailored 
+                compliant with the generation of an overlay cluster tailored 
                 to an use in the aim of accelerator-rich architectures and
                 supporting different accelerator interconnection strategies.
 
@@ -18,15 +18,25 @@
 
 #!/usr/bin/env python3
 
+from templates.ov_templ.hw.cluster.bender.top.bender import Bender
 from templates.ov_templ.hw.cluster.cl_pkg.top.cl_pkg import ClPkg
+from templates.ov_templ.hw.cluster.dmac_wrap_ooc.top.dmac_wrap_ooc import DmacWrapOOC
 from templates.ov_templ.hw.cluster.lic_acc_region.top.lic_acc_region import LicAccRegion
 from templates.ov_templ.hw.cluster.lic_acc_intf.top.lic_acc_intf import LicAccIntf
-from templates.ov_templ.hw.cluster.bender.top.bender import bender
+from templates.ov_templ.hw.cluster.pulp_cluster_ooc.top.pulp_cluster_ooc import PulpClusterOOC
 
 class Cluster:
     def __init__(self):
-        print("\n[py] >> Cluster ~ Retrieving hardware components")
         self.path_common = 'templates/ov_templ/hw/common/'
+
+    def Bender(self):
+        print("\n[py] >> Cluster ~ Bender")
+        return Bender(
+            temp_type = 'templates/ov_templ/hw/cluster/bender/',
+            temp_top = 'bender.template_yml',
+            temp_modules = [],
+            path_common = self.path_common
+        ).top()
 
     def ClPkg(self):
         print("\n[py] >> Cluster ~ Cluster package")
@@ -35,6 +45,15 @@ class Cluster:
             temp_top = 'cl_pkg.template_sv',
             temp_modules = ['hwpe_lic.template_sv',
                             'hwpe_hci.template_sv'],
+            path_common = self.path_common
+        ).top()
+
+    def DmacWrapOOC(self):
+        print("\n[py] >> Cluster ~ DMA wrapper OOC")
+        return DmacWrapOOC(
+            temp_type = 'templates/ov_templ/hw/cluster/dmac_wrap_ooc/',
+            temp_top = 'dmac_wrap_ooc.template_sv',
+            temp_modules = [],
             path_common = self.path_common
         ).top()
 
@@ -57,11 +76,11 @@ class Cluster:
             path_common = self.path_common
         ).top()
 
-    def Bender(self):
-        print("\n[py] >> Cluster ~ Bender")
-        return bender(
-            temp_type = 'templates/ov_templ/hw/cluster/bender/',
-            temp_top = 'bender.template_yml',
+    def PulpClusterOOC(self):
+        print("\n[py] >> Cluster ~ PULP cluster OOC")
+        return PulpClusterOOC(
+            temp_type = 'templates/ov_templ/hw/cluster/pulp_cluster_ooc/',
+            temp_top = 'pulp_cluster_ooc.template_sv',
             temp_modules = [],
             path_common = self.path_common
         ).top()
