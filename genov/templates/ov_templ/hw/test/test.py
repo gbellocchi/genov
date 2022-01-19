49ff082c9cef6d049ff2cb6a5ef6bfd6f719e45d
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-01-19 21:51:27 +0100

    migrated and modified some old system test templates under /test

--- genov/templates/ov_templ/hw/test/test.py
+++ genov/templates/ov_templ/hw/test/test.py
@@ -18,7 +18,8 @@
 
 from templates.ov_templ.hw.test.overlay_tb_hw.top.overlay_tb_hw import OverlayTestbenchHw
 # from templates.ov_templ.hw.test.overlay_tb_sw.top.overlay_tb_sw import OverlayTestbenchSw
-# from templates.ov_templ.hw.test.vsim_wave.top.vsim_wave import VsimWave
+from templates.ov_templ.hw.test.vsim_wave_cluster.top.vsim_wave_cluster import VsimWaveCluster
+from templates.ov_templ.hw.test.vsim_wave_soc.top.vsim_wave_soc import VsimWaveSoc
 
 class Test:
     def __init__(self):
@@ -42,20 +43,33 @@ class Test:
     #         path_common = self.path_common
     #     ).top()
     
-    # def VsimWave(self):
-    #     print("\n[py] >> Overlay ~ QuestaSim waves")
-    #     return VsimWave(
-    #         temp_type = 'templates/ov_templ/hw/test/vsim_wave/',
-    #         temp_top = 'vsim_wave.template_wave_do',
-    #         temp_modules = ['hwpe_ctrl.template_wave_do', 
-    #                         'hwpe_engine.template_wave_do', 
-    #                         'hwpe_fsm.template_wave_do',
-    #                         'hwpe_streamer.template_wave_do',
-    #                         'hwpe_top.template_wave_do',
-    #                         'kernel_interface/mdc_dataflow.template_wave_do',
-    #                         'kernel_interface/xil_ap_ctrl_hs.template_wave_do',
-    #                         'kernel_interface/kernel_hls.template_wave_do',
-    #                         'cl_acc_intf.template_wave_do'
-    #         ],
-    #         path_common = self.path_common
-    #     ).top()
+    def VsimWaveCluster(self):
+        print("\n[py] >> Overlay ~ QuestaSim waves (Cluster)")
+        return VsimWaveCluster(
+            temp_type = 'templates/ov_templ/hw/test/vsim_wave_cluster/',
+            temp_top = 'vsim_wave_cluster.template_wave_do',
+            temp_modules = ['hwpe/hwpe_ctrl.template_wave_do', 
+                            'hwpe/hwpe_engine.template_wave_do', 
+                            'hwpe/hwpe_fsm.template_wave_do',
+                            'hwpe/hwpe_streamer.template_wave_do',
+                            'hwpe/hwpe_top.template_wave_do',
+                            'hwpe/kernel_interface/mdc_dataflow.template_wave_do',
+                            'hwpe/kernel_interface/xil_ap_ctrl_hs.template_wave_do',
+                            'hwpe/kernel_interface/kernel_hls.template_wave_do',
+                            'hwpe.template_wave_do',
+                            'cl_acc_intf.template_wave_do',
+                            'cluster.template_wave_do',
+                            'riscv_core.template_wave_do'
+            ],
+            path_common = self.path_common
+        ).top()
+
+    def VsimWaveSoc(self):
+        print("\n[py] >> Overlay ~ QuestaSim waves (SoC)")
+        return VsimWaveSoc(
+            temp_type = 'templates/ov_templ/hw/test/vsim_wave_soc/',
+            temp_top = 'vsim_wave_soc.template_wave_do',
+            temp_modules = ['soc.template_wave_do'
+            ],
+            path_common = self.path_common
+        ).top()
