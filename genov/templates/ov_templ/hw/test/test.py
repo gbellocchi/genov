c38fdb9544cf18ce2272126a30ce8531571f7afe
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-01-20 12:58:59 +0100

    validated generation of waves up to the LIC accelerator interface. This should be enough for the moment, once the single accelerator wrappers are validated standalone. To have more insight about the inner wrapper components suring system simulation, it is needed to generate also wrapper waves. Not really urgent though now.

--- genov/templates/ov_templ/hw/test/test.py
+++ genov/templates/ov_templ/hw/test/test.py
@@ -19,6 +19,7 @@
 from templates.ov_templ.hw.test.overlay_tb_hw.top.overlay_tb_hw import OverlayTestbenchHw
 # from templates.ov_templ.hw.test.overlay_tb_sw.top.overlay_tb_sw import OverlayTestbenchSw
 from templates.ov_templ.hw.test.vsim_wave_cluster.top.vsim_wave_cluster import VsimWaveCluster
+# from templates.ov_templ.hw.test.vsim_wave_wrapper.top.vsim_wave_wrapper import VsimWaveWrapper
 from templates.ov_templ.hw.test.vsim_wave_soc.top.vsim_wave_soc import VsimWaveSoc
 
 class Test:
@@ -48,28 +49,39 @@ class Test:
         return VsimWaveCluster(
             temp_type = 'templates/ov_templ/hw/test/vsim_wave_cluster/',
             temp_top = 'vsim_wave_cluster.template_wave_do',
-            temp_modules = ['hwpe/hwpe_ctrl.template_wave_do', 
-                            'hwpe/hwpe_engine.template_wave_do', 
-                            'hwpe/hwpe_fsm.template_wave_do',
-                            'hwpe/hwpe_streamer.template_wave_do',
-                            'hwpe/hwpe_top.template_wave_do',
-                            'hwpe/kernel_interface/mdc_dataflow.template_wave_do',
-                            'hwpe/kernel_interface/xil_ap_ctrl_hs.template_wave_do',
-                            'hwpe/kernel_interface/kernel_hls.template_wave_do',
-                            'hwpe.template_wave_do',
-                            'cl_acc_intf.template_wave_do',
-                            'cluster.template_wave_do',
+            temp_modules = ['cluster.template_wave_do',
+                            'lic_acc_region.template_wave_do',
                             'riscv_core.template_wave_do'
             ],
             path_common = self.path_common
         ).top()
+
+    # def VsimWaveWrapper(self):
+    #     print("\n[py] >> Overlay ~ QuestaSim waves (Wrapper)")
+    #     return VsimWaveWrapper(
+    #         temp_type = 'templates/ov_templ/hw/test/vsim_wave_wrapper/',
+    #         temp_top = 'vsim_wave_wrapper.template_wave_do',
+    #         temp_modules = ['hwpe/hwpe_ctrl.template_wave_do', 
+    #                         'hwpe/hwpe_engine.template_wave_do', 
+    #                         'hwpe/hwpe_fsm.template_wave_do',
+    #                         'hwpe/hwpe_streamer.template_wave_do',
+    #                         'hwpe/hwpe_top.template_wave_do',
+    #                         'hwpe/kernel_interface/mdc_dataflow.template_wave_do',
+    #                         'hwpe/kernel_interface/xil_ap_ctrl_hs.template_wave_do',
+    #                         'hwpe/kernel_interface/kernel_hls.template_wave_do',
+    #                         'hwpe.template_wave_do'
+    #         ],
+    #         path_common = self.path_common
+    #     ).top()
 
     def VsimWaveSoc(self):
         print("\n[py] >> Overlay ~ QuestaSim waves (SoC)")
         return VsimWaveSoc(
             temp_type = 'templates/ov_templ/hw/test/vsim_wave_soc/',
             temp_top = 'vsim_wave_soc.template_wave_do',
-            temp_modules = ['soc.template_wave_do'
+            temp_modules = ['pulp_cluster_inputs.template_wave_do',
+                            'pulp_cluster_outputs.template_wave_do',
+                            'soc.template_wave_do'
             ],
             path_common = self.path_common
         ).top()
