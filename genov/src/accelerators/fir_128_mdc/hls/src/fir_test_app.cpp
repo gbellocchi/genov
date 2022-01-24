c293eafc30f33bccd834fc06ad4b1894ddeea133
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-01-24 21:14:53 +0100

    added fir_128_mdc in place of fir_app_mdc

---
+++ genov/src/accelerators/fir_128_mdc/hls/src/fir_test_app.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "fir.h"
+
+#include "inc/x_V.h"
+#include "inc/y_V_ref.h"
+// #include "inc/taps.h"
+
+int main()
+{
+  size_t n_samples = N_SAMPLES;
+  size_t n_coeffs = TAP;
+
+  data_t din[n_samples];
+  data_t dout_ref[n_samples];
+  data_t dout_dut = 0;
+
+  data_t coeffs[n_coeffs];
+
+  int err_cnt = 0;
+
+  // Create test data
+  for (int i = 0; i < n_samples; i++) {
+    din[i] = x_V[i];
+    dout_ref[i] = y_V_ref[i];
+  }
+
+  // // Create filter coeffs
+  // for (int i = 0; i < n_coeffs; i++) {
+  //   coeffs[i] = taps[i];
+  // }
+
+  //Launch the hardware solution
+  for (int i = 0; i < n_samples; i++) {
+    // fir(din[i], coeffs, dout_dut);
+    fir(din[i], dout_dut);
+
+    // error check
+    std::cout << "Iter #" << i << std::endl;
+    std::cout << "dout_dut = " << dout_dut << std::endl;
+    std::cout << "dout_ref = " << dout_ref[i] << std::endl;
+
+    if( dout_dut != dout_ref[i] ){
+      err_cnt++;
+    }
+  }
+
+  int ret_val = 0;
+  
+  if (err_cnt == 0) {
+    cout << "*** TEST PASSED ***" << endl;
+    int ret_val = 0;
+  } else {
+    cout << "!!! TEST FAILED - " << err_cnt << " mismatches detected !!!";
+    cout << endl;
+    int ret_val = -1;
+  }
+
+  return ret_val;
+}
---
+++ genov/src/accelerators/fir_128_mdc/hls/src/fir_test_toy.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <fstream>
+
+#include "fir.h"
+
+int main()
+{
+  data_t X[20] = {10, -4, 1, 7, 1, 1, 0, 0, 1, 1, 1, -6, -2, -1, 2, 2, 0, 0, 0, 0};
+  coeff_t H[TAP] = {1, 2, 2, 1};
+  sum_t Y = 0;
+  int retval=0;
+  ofstream FILE;
+
+  // Save the results to a file
+  FILE.open ("result.dat");
+
+  ofstream myfile;
+  std::stringstream stream;
+  myfile.open ("golden_results.txt");
+
+  for (int i = 0; i < 20; i++)
+  {
+    fir(X[i], H, Y);
+    cout << "Iter:" << setw(3) << right << i << " with x" << setw(3) << right
+    << X[i] << " ###:" << setw(3) << right << Y << endl;
+    FILE << Y << endl;
+    // write results to file
+    stream << std::hex << Y;
+    myfile << stream.str();
+    myfile << endl;
+    stream.clear();
+    stream.str("");
+  }
+
+  FILE.close();
+  myfile.close();
+
+  // Compare the results file with the golden results
+  retval = system("diff --brief -w result.dat result.golden.dat");
+  if (retval != 0) {
+    cout << "Test failed  !!!" << endl; 
+    retval=1;
+  } else {
+    cout << "Test passed !" << endl;
+  }
+
+  // Return 0 if the test
+  return retval;
+}
